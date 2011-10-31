aa743a3215b0e96684eb365bf4c9d2880e8eed7e
Magnus Nordlander <magnus@nordlander.se>
2011-10-31 23:11:07 +0100

    Used ArrayCollection

--- src/Stoxs/Bundle/AppBundle/Entity/User.php
+++ src/Stoxs/Bundle/AppBundle/Entity/User.php
@@ -5,6 +5,8 @@ namespace Stoxs\Bundle\AppBundle\Entity;
 use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
+use Doctrine\Common\Collections\ArrayCollection;
+
 /**
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
